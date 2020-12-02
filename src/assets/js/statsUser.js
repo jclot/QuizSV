3e18c9de5e34eb03d9897318841221916da7cfe2
Julian Clot <JulianClot@Julians-MacBook-Pro.local>
2020-12-02 02:31:30 -0600

    Semi-create an stats user procces

---
+++ src/assets/js/statsUser.js
@@ -0,0 +1,70 @@
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
+const { spawn } = require("child_process");
+
+
+var release_os = os.release(); // does not used it until the program is finished; 
+var plataform_os = os.platform();
+
+var dir_home = process.env [process.platform == "win32"?"USERPROFILE":"HOME"];
+var dir_desktop = path.join(dir_home, "Desktop");
+var admin_user = os.userInfo().username;
+
+function get_app_procces(app) {
+
+ 
+
+}
+
+function file_content() {
+
+  
+
+}
+
+
+function info_file() {
+
+if(plataform_os === "darwin") {
+
+    console.log("This user is OSX");
+    fs.writeFile(dir_desktop + "/" + admin_user + ".txt", 'asdas', function(err) {
+        if(err) {
+           return console.log(err);
+        } else {
+            return console.log("The file was saved!!");
+        }
+        
+    });
+    
+    
+} else if(plataform_os === "win32") {
+
+    console.log("This user is Windows");
+    fs.writeFile(dir_desktop + "/" + admin_user + ".txt", 'testFile', function(err) {
+        if(err) {
+           return console.log(err);
+        } else {
+            return console.log("The file was saved!!");
+        }
+        
+    });
+
+} else if(plataform_os === "linux") {
+
+    console.log("This user is Linux");
+    fs.writeFile(dir_desktop + "/" + admin_user + ".txt", 'testFile', function(err) {
+        if(err) {
+           return console.log(err);
+        } else {
+            return console.log("The file was saved!!");
+        }
+        
+    });
+
+  }
+
+}
+
+// info_file();
