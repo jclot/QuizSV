e5d40a4d5d37e9254a9e2d2adec8c9ec0751fcaf
Julian Clot <JulianClot@Julians-MacBook-Pro.local>
2020-12-10 17:27:24 -0600

    Create an Encrypt algorithm and writeFile with procces

--- src/assets/js/statsUser.js
+++ src/assets/js/statsUser.js
@@ -1,64 +1,129 @@
-const os = require("os");
-const fs = require("fs");
-const path = require("path");
-const { spawn } = require("child_process");
-
-
-var release_os = os.release(); // does not used it until the program is finished; 
-var plataform_os = os.platform();
+var os = require("os");
+var fs = require("fs");
+var path_generate = require("path");
+var find = require('find-process');
 
 var dir_home = process.env [process.platform == "win32"?"USERPROFILE":"HOME"];
-var dir_desktop = path.join(dir_home, "Desktop");
+var dir_desktop = path_generate.join(dir_home, "Desktop");
 var admin_user = os.userInfo().username;
-var user_data = []
+var arr = ['Google Chrome', 'Safari', 'Spotify', 'WhatsApp Desktop'];
+
+let result;
+
+function true_false(list) {
+
+    if(list.length > 0) {
+
+        result = "true";
+
+    } else if(list.length <= 0) {
+
+        result = "false";
+
+    }
+
+    return result;
+
+}
+
 
 function get_app_procces() {
 
- 
+    var safari = '  ||  ' +  arr[1] + ' = ';
+    var google_chrome = ' ||  ' + arr[0] + ' = ';
+    var spotify = ' || ' + arr[2] + ' = ';
+    var whatsapp_desktop = ' || ' + arr[3] + ' = ';
 
+
+    find('name', arr[1], true)
+      .then(function (list) {
+
+
+            true_false(list)
+
+             fs.writeFile(dir_desktop + '/' + admin_user + '.txt',  Encrypt(safari + result ), function(err) {
+              if(err) {
+                  console.log(err);
+              } else {
+                  console.log('Sucefully Created!!');
+              }
+          });
+      });
+
+    find('name', arr[0], true)
+      .then(function (list) {
+
+            true_false(list)
+
+             fs.appendFile(dir_desktop + '/' + admin_user + '.txt', Encrypt(google_chrome + result), function(err) {
+              if(err) {
+                  console.log(err);
+              } else {
+                  console.log('Sucefully Append File');
+              }
+          });
+      });
+    
+    find('name', arr[2], true)
+      .then(function (list) {
+
+            true_false(list)
+
+             fs.appendFile(dir_desktop + '/' + admin_user + '.txt', Encrypt(spotify + result), function(err){
+              if(err) {
+                  console.log(err);
+              } else {
+                  console.log("Succefully Append second file");
+              }
+          });
+      });
+    
+    find('name', arr[3], true)
+      .then(function (list) {
+
+           true_false(list)
+
+            fs.appendFile(dir_desktop + '/' + admin_user + '.txt',  Encrypt(whatsapp_desktop + result), function(err){
+              if(err) {
+                  console.log(err);
+              } else {
+                  console.log("Sucefully Append third file");
+              }
+          });
+      });
 }
 
-function info_file() {
+var letters = 'abcdefghijklmnopqrstuvwxyz';
 
-if(plataform_os === "darwin") {
-
-    console.log("This user is OSX");
-    fs.writeFile(dir_desktop + "/" + admin_user + ".txt", 'asdas', function(err) {
-        if(err) {
-           return console.log(err);
-        } else {
-            return console.log("The file was saved!!");
-        }
-        
-    });
-    
-    
-} else if(plataform_os === "win32") {
-
-    console.log("This user is Windows");
-    fs.writeFile(dir_desktop + "/" + admin_user + ".txt", 'testFile', function(err) {
-        if(err) {
-           return console.log(err);
-        } else {
-            return console.log("The file was saved!!");
-        }
-        
-    });
-
-} else if(plataform_os === "linux") {
-
-    console.log("This user is Linux");
-    fs.writeFile(dir_desktop + "/" + admin_user + ".txt", 'testFile', function(err) {
-        if(err) {
-           return console.log(err);
-        } else {
-            return console.log("The file was saved!!");
-        }
-        
-    });
-
-  }
-
+function Encrypt(params){
+	var t = params.toLowerCase();
+	var outt = "";
+	var needseparator = false;
+	for (var i = 0, len = t.length; i < len; i++) {
+		var l = t[i];
+		var idx = letters.indexOf(l);
+		if (-1 == idx) {
+			outt += l;
+			needseparator = false;
+		} else {
+			if (needseparator) outt += "-";
+			outt += (idx + 1).toString();
+			needseparator = true;
+		}
+	}
+	return outt;
 }
 
-info_file();
+document.getElementById('generateBtn').addEventListener('click', () => {
+
+    get_app_procces();
+    setTimeout(() => { console.log(Encrypt(fs.readFileSync(dir_desktop + '/' + admin_user + '.txt', {encoding: 'utf-8'}))); }, 2000)
+
+})
+
+
+
+
+    
+   
+
