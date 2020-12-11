60378086bc14641b79336ba155470c10c1a68ea7
DESKTOP-931PEJ2\JulianClot <juliclot123@gmail.com>
2020-12-11 01:08:03 -0600

    Delete the dragable element for windows.

--- src/assets/js/shortcuts.js
+++ src/assets/js/shortcuts.js
@@ -1,8 +1,16 @@
 const { desktopCapturer, remote } = require('electron');
+const { platform } = require('os');
+const dragable = document.getElementById('dragable');
 
 const backButton = document.getElementById('backInstructionsButton');
 backButton.onclick = () => {
-  
-  remote.getCurrentWindow().close();
-  remote.getCurrentWindow().setFocusable(true);
+
+    remote.getCurrentWindow().close();
+    remote.getCurrentWindow().setFocusable(true);
+}
+
+if (process.platform === "win32") {
+
+    dragable.remove();
+
 }
