1a96a14c0d263a6c00109ec978e638a23566b2d2
DESKTOP-931PEJ2\JulianClot <juliclot123@gmail.com>
2020-12-11 01:08:50 -0600

    Create a specific dimesnion for instructions window in win32

--- src/pages/screens/createWindow.js
+++ src/pages/screens/createWindow.js
@@ -1,5 +1,7 @@
+const { Menu } = require('electron');
 const electron = require('electron');
 const BrowserWindow = electron.remote.BrowserWindow;
+const { platform } = require('os');
 
 
 
@@ -10,35 +12,48 @@ const path_file = require('path');
 let InstructionsScreen;
 
 const createWindowInstructions = () => {
-    // Create the browser window.
-     InstructionsScreen = new BrowserWindow({
-        titleBarStyle: 'hidden',
-        width: 800,
-        height: 700,
-        maxWidth: 800,
-        maxHeight: 700,
-        webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModule: true,
-        },
-        fullscreen: false,
-        backgroundColor: "#fff",
-      
 
+    if (process.platform === "darwin") {
 
-    });
+        // Create the browser window.
+        InstructionsScreen = new BrowserWindow({
+            titleBarStyle: 'hidden',
+            width: 800,
+            height: 700,
+            maxWidth: 800,
+            maxHeight: 700,
+            webPreferences: {
+                nodeIntegration: true,
+                enableRemoteModule: true,
+            },
+            fullscreen: false,
+            backgroundColor: "#fff",
+        });
 
+    } else if (process.platform === "win32") {
 
+        InstructionsScreen = new BrowserWindow({
+            titleBarStyle: 'hidden',
+            width: 670,
+            height: 760,
+            maxWidth: 670,
+            maxHeight: 760,
+            webPreferences: {
+                nodeIntegration: true,
+                enableRemoteModule: true,
+            },
+            fullscreen: false,
+            backgroundColor: "#fff",
+        });
+
+    }
     InstructionsScreen.loadFile(path_file.join(__dirname, 'pages/Instructions.html'));
 
 };
+
 
 button.addEventListener("click", function() {
 
     createWindowInstructions();
 
 })
-
-
-
-
